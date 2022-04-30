8ee06cfcb44d38c0e2da6ec8f1a7fd082c8c52ae
muhammadqasemtarboush1 <muhammadtarboush1@gmail.com>
2022-04-30 06:12:49 +0300

    update: add new endpoints and use themoviedb api

--- README.md
+++ README.md
@@ -6,11 +6,21 @@
 
 ![WRRC](./WRRC.png)
 
+Web Request Response Cycle with 3th party api
+
+![WRRC3TH](./WRRC3TH.jpg)
+
 ## Overview
 
 a movie app that can check the latest movies based on categories
 
 ## Getting Started
+
+This project build with:
+
+- Node.js
+- Express.js
+- 3th Party API - The Movie DB
 
 Before starting type into your terminal:
 
@@ -27,3 +37,7 @@ We have the following routes :
 - / , will lead you to the move object
 - /favorite , for your favorites movies
 - /err , for testing errors "dev purpose"
+- /search , Search for a movie name to get its information
+- /trending, Get the trending movies data from the Movie DB API
+- /tv , Get the list of official genres for TV shows.
+- /changes,Get a list of all of the movie ids that have been changed in the past 24 hours
--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require("express");
-const data = require("./Movie Data/data.json");
+const cors = require("cors");
+const axios = require("axios").default;
+require("dotenv").config();
+const PORT = 3000;
 const app = express();
-const port = 3000;
+app.use(cors());
+let apiKey = process.env.API_KEY;
 
 // Home page
 app.get("/", HandleMoive);
+
+// @rout
+// @description: Trending Movie
+app.get("/trending", getTrendingMovies);
+// @rout
+// @description: Search by movie name
+app.get("/search", searchMovies);
+// @rout
+// @description: Favorite page
+app.get("/tv", getGenres);
+// @rout
+// @description: Movie changed in the past 24 hours.
+app.get("/changes", getChanges);
 
 // @rout
 // @description: Favorite page
@@ -37,8 +54,83 @@ app.use(function (err, req, res, next) {
 // Controller Home page
 function HandleMoive(req, res) {
   let movie = new Movie(data.title, data.poster_path, data.overview);
-  console.log(movie);
   res.json(movie);
+}
+
+// * Task 12 *//
+function getTrendingMovies(req, res) {
+  let url = `https://api.themoviedb.org/3/trending/all/week?api_key=${apiKey}`;
+  axios
+    .get(url)
+    .then((result) => {
+      // res.json(result.data.results);
+      // throw new Error
+      let movies = result.data.results.map((movie) => {
+        return new Movie(
+          movie.id,
+          movie.title,
+          movie.release_date,
+          movie.poster_path,
+          movie.overview
+        );
+      });
+      res.json(movies);
+    })
+    .catch((error) => {
+      res.send("error in getting data from API");
+    });
+}
+
+function searchMovies(req, res) {
+  let q_name = req.query.name;
+  let url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${q_name}&page=1`;
+  axios
+    .get(url)
+    .then((result) => {
+      let movieArray = result.data.results.map((movie) => {
+        return new Movie(
+          movie.id,
+          movie.title,
+          movie.release_date,
+          movie.poster_path,
+          movie.overview
+        );
+      });
+      res.json(movieArray);
+    })
+    .catch((error) => {
+      res.json(error);
+    });
+}
+
+function getGenres(req, res) {
+  let url = `https://api.themoviedb.org/3/genre/tv/list?api_key=${apiKey}&language=en-US`;
+  axios
+    .get(url)
+    .then((result) => {
+      let tvGenre = result.data.genres.map((genre) => {
+        return new TVGenre(genre.id, genre.name);
+      });
+      res.json(tvGenre);
+    })
+    .catch((error) => {
+      res.send(error);
+    });
+}
+
+function getChanges(req, res) {
+  let url = `https://api.themoviedb.org/3/movie/changes?api_key=${apiKey}&page=1`;
+  axios
+    .get(url)
+    .then((result) => {
+      let changes = result.data.results.map((changes) => {
+        return new Changes(changes.id, changes.adult);
+      });
+      res.json(changes);
+    })
+    .catch((error) => {
+      res.send(error);
+    });
 }
 
 // Constructer function
@@ -48,7 +140,21 @@ function Movie(title, poster_path, overview) {
   this.overview = overview;
 }
 
-let PORT = process.env.PORT || 3000;
+function Movie(id, title, release_date, poster_path, overview) {
+  (this.id = id),
+    (this.title = title),
+    (this.release_date = release_date),
+    (this.poster_path = poster_path),
+    (this.overview = overview);
+}
+
+function TVGenre(id, name) {
+  (this.id = id), (this.name = name);
+}
+
+function Changes(id, adult) {
+  (this.id = id), (this.adult = adult);
+}
 
 // runing the server
 app.listen(PORT, () => {
