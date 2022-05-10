4588db733db3eb0e52838252e99e95be706ef3a1
muhammadqasemtarboush1 <muhammadtarboush1@gmail.com>
2022-05-10 13:46:21 +0300

    fix: nodemon:

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const axios = require("axios").default;
 const bodyParser = require("body-parser");
 require("dotenv").config();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const data = require("./Movie Data/data.json");
 const app = express();
 const morgan = require("morgan");
